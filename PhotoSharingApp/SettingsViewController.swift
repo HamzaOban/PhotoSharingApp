7ca7aaabdbad9658e4f10e0487a7bab24544acdb
Hamza Oban <hamzaoban3@gmail.com>
2022-10-07 18:02:01 +0300

    init

---
+++ PhotoSharingApp/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  PhotoSharingApp
+//
+//  Created by Hamza Oban on 2.10.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func cikisYapOnClick(_ sender: Any) {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PhotoSharingApp/ViewController.swift
+++ PhotoSharingApp/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sifreTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func kayitOlOnClick(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
+    
+    @IBAction func girisYapOnClick(_ sender: Any) {
+    }
 }
 
